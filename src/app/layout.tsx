e49e3579b98eaafb9c3e20edc9c5dd13d0fcc72b
berkaydenizyilmaz <yilmazberkaydeniz@gmail.com>
2025-07-20 22:38:02 +0300

    Oturum yönetimi için SessionProvider bileşeni layout'a eklendi. Bu değişiklik, uygulamanın oturum yönetimini iyileştirmek amacıyla gerçekleştirildi.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { SessionProvider } from "@/components/providers/session-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryProvider>
-          <ThemeProvider>
-            {children}
-          </ThemeProvider>
-        </QueryProvider>
+        <SessionProvider>
+          <QueryProvider>
+            <ThemeProvider>
+              {children}
+            </ThemeProvider>
+          </QueryProvider>
+        </SessionProvider>
       </body>
     </html>
   );
---
+++ src/components/providers/session-provider.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
+import { type SessionProviderProps } from 'next-auth/react';
+
+export function SessionProvider({ children, ...props }: SessionProviderProps) {
+  return (
+    <NextAuthSessionProvider {...props}>
+      {children}
+    </NextAuthSessionProvider>
+  );
+} 
