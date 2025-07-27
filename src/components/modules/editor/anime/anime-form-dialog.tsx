129aecf8f348a5f4149c4b39ffce2dc8dc49c09f
berkaydenizyilmaz <yilmazberkaydeniz@gmail.com>
2025-07-27 12:24:17 +0300

    Anime form diyalog bileşeninde içerik alanının stilinde güncelleme yapıldı. Genişlik ayarları için "!w-[98vw]" ve "!max-w-7xl" sınıfları eklendi.

--- src/components/modules/editor/anime/anime-form-dialog.tsx
+++ src/components/modules/editor/anime/anime-form-dialog.tsx
@@ -197,7 +197,7 @@ export function AnimeFormDialog({ open, onOpenChange, anime, onSuccess }: AnimeF
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="w-[98vw] max-w-7xl max-h-[90vh] overflow-y-auto">
+      <DialogContent className="!w-[98vw] !max-w-7xl max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>
             {isEdit ? 'Anime Düzenle' : 'Yeni Anime Ekle'}
